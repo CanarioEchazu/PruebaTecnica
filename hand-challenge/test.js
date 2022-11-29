4d795d0ebc6edcf14eee4d66661386c8a70ab477
Dario Canario Echazu <105083756+CanarioEchazu@users.noreply.github.com>
2022-11-29 20:40:44 -0300

    Add files via upload

---
+++ hand-challenge/test.js
@@ -0,0 +1,13 @@
+/* global test expect */
+
+const { TestWatcher } = require('jest-watcher');
+const { default: test } = require('node:test');
+const translate = require('./index.js');
+
+test('it translate "Hello" accordinly', () => {
+    expect(translate('👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊')).toBe('Hello');
+})
+
+test('it translate "Hello World !" accordinly', () => {
+    expect(translate('👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊')).toBe('Hello World !');
+})
